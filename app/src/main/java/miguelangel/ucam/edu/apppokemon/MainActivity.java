ed60c094dbd647506306229cee096d340b0021df
Miguel Angel <miguelm37@gmail.com>
2017-01-14 21:10:54 +0100

    Initial commit

---
+++ app/src/main/java/miguelangel/ucam/edu/apppokemon/MainActivity.java
@@ -0,0 +1,13 @@
+package miguelangel.ucam.edu.apppokemon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
